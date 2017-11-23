b343903aa0bb9fb14a77a52abc13b2a584499dba
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2017-11-23 19:48:19 +0000

    Support structures for importing Henshin files from Xtext

---
+++ uk.ac.kcl.inf.xdsml_compose/src/uk/ac/kcl/inf/henshin/HenshinRuntimeModule.java
@@ -0,0 +1,23 @@
+package uk.ac.kcl.inf.henshin;
+
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
+import org.eclipse.xtext.resource.generic.AbstractGenericResourceRuntimeModule;
+
+public class HenshinRuntimeModule extends AbstractGenericResourceRuntimeModule {
+
+	@Override
+	protected String getLanguageName() {
+		return "org.eclipse.emf.henshin.presentation.HenshinEditorID";
+	}
+
+	@Override
+	protected String getFileExtensions() {
+		return "henshin";
+	}
+
+	@Override
+	public Class<? extends IQualifiedNameProvider> bindIQualifiedNameProvider() {
+		return HenshinQualifiedNameProvider.class;
+	}
+}
